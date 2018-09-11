767fd7fabb4c8a432c950c9144b801afa5e77fa2
Mohammed Abdulkereem <mohammed-io@outlook.com>
2018-09-11 16:51:11 +0300

    Start using gatsby

---
+++ path----04db98179d6562ab8157.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{104:function(t,a){t.exports={data:{site:{siteMetadata:{title:"My Amazing Portfolio"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----04db98179d6562ab8157.js.map
